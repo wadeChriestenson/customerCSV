9c603bba04669aa3befa40e9050164c28c215691
Wade Chriestenson <wadechriestenson@gmail.com>
2021-12-26 15:35:12 -0800

    Update fakeData.py

--- fakeData.py
+++ fakeData.py
@@ -5,14 +5,20 @@ import pandas as pd
 
 # Create a Faker instance
 fake = Faker()
-
-# Create a csv file
+# print(fake.first_name())
+# print(fake.last_name())
+# print(fake.email())
+# print(fake.phone_number())
+# print(fake.address())
+# print(fake.job())
+# print(fake.company())
+# print(fake.country())
+# print(fake.text())
+# Set the headers
+fields = ['First Name', 'Last Name', 'Email', 'Phone Number', 'Address', 'Country', 'Job', 'Company Location', 'Bio']
+# # # Create a csv file
 fileName = 'cust.csv'
 
-# Set the headers
-fields = ['Name', 'Email', 'Address', 'Country', 'Company']
-
-# Open file and write the csv
 with open(fileName, 'w') as csvFile:
     csvwriter = csv.writer(csvFile)
     csvwriter.writerow(fields)
@@ -20,13 +26,19 @@ with open(fileName, 'w') as csvFile:
     # Loop through the fake data to create all the entries
     #  While is number of rows you want in you csv file.
     while x <= 100000:
-        csvwriter.writerows([[fake.name(),
-                             fake.email(),
-                             fake.address(),
-                             fake.country(),
-                             fake.company()]])
+        csvwriter.writerows([[
+            fake.first_name(),
+            fake.last_name(),
+            fake.email(),
+            fake.phone_number(),
+            fake.address(),
+            fake.country(),
+            fake.job(),
+            fake.company(),
+            fake.text()
+        ]])
         x += 1
 
-# Open the CSV and removes all the empty rows and makes a new output CSV call Customer.csv
+# # Open the CSV and removes all the empty rows and makes a new output CSV call Customer.csv
 df = pd.read_csv(fileName)
 df.to_csv('Customer.csv', index=False)
